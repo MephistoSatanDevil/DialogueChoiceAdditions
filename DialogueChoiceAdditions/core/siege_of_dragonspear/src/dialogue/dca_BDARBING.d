ca9f9465df82bc6f58891e645e8d5b9f6ea6d315
virginviolet <148799361+virginviolet@users.noreply.github.com>
2026-04-30 17:40:37 +0200

    Changed translation string number for dca_BDARBING

--- DialogueChoiceAdditions/core/siege_of_dragonspear/src/dialogue/dca_BDARBING.d
+++ DialogueChoiceAdditions/core/siege_of_dragonspear/src/dialogue/dca_BDARBING.d
@@ -5,5 +5,5 @@
 ~Not your fight, <PRO_RACE>. Leave before Arbinge smash you to blood 'n bits!~ */
 EXTEND_BOTTOM ~%bdarbing%~ 7
     // Uninterested/respectful/risk-averse
-    IF ~~ THEN REPLY @2002 /* ~I know good counsel when I hear it. Goodbye.~ */ GOTO 10
+    IF ~~ THEN REPLY @3000 /* ~I know good counsel when I hear it. Goodbye.~ */ GOTO 10
 END
