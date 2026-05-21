b092b66d004ba6036852d1b64da6586c01653f54
virginviolet <148799361+virginviolet@users.noreply.github.com>
2026-05-21 18:17:22 +0200

    Revert "Prevented Imoen dialogue about Her room if they know about the room"
    This reverts commit ce55544cae3fe4fdb2c4a58c570571fbcaf3a3d1.

--- DialogueChoiceAdditions/core/shadows_of_amn/src/dialogue/dca_her_room.d
+++
@@ -1,7 +0,0 @@
-/* Imoen: ~What...? What is this place? It's—it's beautiful! Oh... there is simply no way that this space belongs to the same person that owns those horrible glass containers.~ */
-
-// Prevent this state from triggering if a dryad has mentioned Her room,
-// because she likely wouldn't say this then. And if she did, the player should
-// be able to say, "It must be the room the dryad mentioned", but none of the
-// followups fit that response.
-ADD_STATE_TRIGGER IMOEN2J 1530 ~!Global("HelpDryads","GLOBAL",2)~
